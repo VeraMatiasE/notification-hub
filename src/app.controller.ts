85e9a62f96ab8f24055d0b68bb57e37d8582154b
Vera Matías <89822359+VeraMatiasE@users.noreply.github.com>
2026-06-02 12:17:18 -0300

    chore: add healthz endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,12 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('healthz')
+  healthCheck() {
+    return {
+      status: 'ok',
+      timestamp: new Date().toISOString(),
+    };
+  }
 }
